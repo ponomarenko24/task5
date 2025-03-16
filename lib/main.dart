f65cb3495f41eca834e89c8795d168f9ed19ecbc
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-03-16 22:06:46 +0200

    Filled containers, Added photo

--- lib/main.dart
+++ lib/main.dart
@@ -48,28 +48,22 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          spacing: 20,
+          // spacing: 20,
           children: <Widget>[
             Text(
               "Beautiful Images",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            Container(
-              width: 100,
-              height: 100,
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey
-                ),
-                borderRadius: BorderRadius.circular(10),
-                
-              ),
+            photoContainer(
+              imageURL:
+                  'https://fastly.picsum.photos/id/179/2048/1365.jpg?hmac=GJyDjrvfBfjPfJPqSBd2pX6sjvsGbG10d21blr5bTS8',
             ),
-           ],
+            SizedBox(height: 20),
+            photoContainer(
+              imageURL:
+                  'https://fastly.picsum.photos/id/200/1920/1280.jpg?hmac=-eKjMC8-UrbLMpy1A4OWrK0feVPB3Ka5KNOGibQzpRU',
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -77,6 +71,17 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Container photoContainer({required String imageURL}) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Image.network(imageURL, width: 200, height: 200),
     );
   }
 }
